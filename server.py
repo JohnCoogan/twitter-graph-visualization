06e7185d002804a8d96b90fe7888b83668f303cf
John Coogan <coogan.johna@gmail.com>
2012-12-02 05:30:59 -0800

    fixed port handling for deployment

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,6 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('graph.html')
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
